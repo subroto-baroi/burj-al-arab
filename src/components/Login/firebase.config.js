47805a822fd502e09adcb8e6102c86105a74eac2
subroto-baroi <67515709+subroto-baroi@users.noreply.github.com>
2020-09-13 17:04:01 +0600

    add burj al arab project

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBh-CRTzftjQYNdNcgEoZTJYR3RB70pXpY",
+    authDomain: "burj-al-arab-f4867.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-f4867.firebaseio.com",
+    projectId: "burj-al-arab-f4867",
+    storageBucket: "burj-al-arab-f4867.appspot.com",
+    messagingSenderId: "1051980793174",
+    appId: "1:1051980793174:web:5199be7b5c11f80ef202f7"
+  };
+
+  export default firebaseConfig;
